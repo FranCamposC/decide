6e170a2fa1973241d66e4fc5f301a805deb572b4
Joaquin-Arregui <joaquin.arregui2002@gmail.com>
2023-12-16 12:05:25 +0100

    feat: Añadida url para editar censos

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.CensusCreate.as_view(), name='census_create'),
     path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),
-    path('list/', views.CensusList.as_view(), name='census_list')
+    path('list/', views.CensusList.as_view(), name='census_list'),
+    path('edit/<int:voting_id>/', views.editCensus(), name='edit_census')
 ]
