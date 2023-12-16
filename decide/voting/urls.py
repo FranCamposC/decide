ed80774b8a5bcabb58d823734e52661d3ea4fab4
[[JoseLuisCobo17]] <[[josecobari@alum.us.es]]>
2023-12-16 12:42:21 +0100

    feat: Urls para eliminar una votacion

--- decide/voting/urls.py
+++ decide/voting/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.VotingView.as_view(), name='voting'),
     path('<int:voting_id>/', views.VotingUpdate.as_view(), name='voting'),
     path('list',views.VotingListView, name="list"), 
-    path('question/list',views.ListQuestion.as_view(), name='questionList')
+    path('question/list',views.ListQuestion.as_view(), name='questionList'),
+    path('delete/<int:voting_id>',views.VotingDeleteView , name="votingDelete"),
 
 ]
