8674275f5bf1ae6e1d70a43f51cd8cc6893b5be3
[[JoseLuisCobo17]] <[[josecobari@alum.us.es]]>
2023-12-16 13:36:53 +0100

    feat: Url para editar votacion

--- decide/voting/urls.py
+++ decide/voting/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('question/delete/<int:question_id>',views.QuestionDeleteView, name='delete'),
     path('list',views.VotingListView, name="list"), 
     path('delete/<int:voting_id>',views.VotingDeleteView , name="votingDelete"),
+    path('edit/<int:voting_id>',views.VotingEditView , name="votingEdit"),
 
 ]
