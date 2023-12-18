d80ebab3f8a5e0e22dcaf5b347262d3292cea756
[[nicoloboo02]] <[[nicherlob@alum.us.es]]>
2023-12-18 15:05:55 +0100

    feat: Acabado del script de configuración

--- decide/start.py
+++ decide/start.py
@@ -5,6 +5,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'decide.settings')
 django.setup()
 
 from factory import Factory,LazyAttribute
+import subprocess
 from factory.django import DjangoModelFactory
 from faker import Faker
 from django.contrib.auth.models import User
@@ -16,18 +17,8 @@ import time
 from django.db.models import TextField
 from census.models import Census
 
-fake = Faker("es_ES")
-
-
-# class UserFactory(DjangoModelFactory):
-#     class Meta:
-#         model = User
-
-#     username = LazyAttribute(lambda x: fake.user_name()+str(random.randint(1,100)))
-#     email = fake.email()
-#     password = TextField(default="1234")
-
 preguntas_predefinidas=[
+
     {"question_desc": "¿Cual es tu opinion sobre el cambio climatico?",
      "opciones": [
          {"number": 1, "option": "Muy preocupado/a"},
@@ -36,37 +27,43 @@ preguntas_predefinidas=[
          {"number": 4, "option": "No preocupado/a"}
      ]},
 
-    {"question_desc": "¿Cual es tu opinion sobre el aborto?",
-     "opciones": [
-         {"number": 1, "option": "A favor"},
-         {"number": 2, "option": "En contra"},
-         {"number": 3, "option": "Neutral"},
-         {"number": 4, "option": "No estoy seguro/a"}
-     ]},
+    {
+        "question_desc": "¿Cuál es tu comida favorita?",
+        "opciones": [
+            {"number": 1, "option": "Pizza"},
+            {"number": 2, "option": "Ensalada"},
+            {"number": 3, "option": "Pasta"},
+            {"number": 4, "option": "Sushi"}
+        ]
+    },
+    {
+        "question_desc": "¿Prefieres la playa o la montaña?",
+        "opciones": [
+            {"number": 1, "option": "Playa"},
+            {"number": 2, "option": "Montaña"},
+            {"number": 3, "option": "Ambos"},
+            {"number": 4, "option": "No tengo preferencia"}
+        ]
+    },
+    {
+        "question_desc": "¿Cuál es tu género literario favorito?",
+        "opciones": [
+            {"number": 1, "option": "Ficción"},
+            {"number": 2, "option": "No ficción"},
+            {"number": 3, "option": "Ciencia ficción"},
+            {"number": 4, "option": "Misterio"}
+        ]
+    },
+    {
+        "question_desc": "¿Prefieres ver películas en casa o en el cine?",
+        "opciones": [
+            {"number": 1, "option": "En casa"},
+            {"number": 2, "option": "En el cine"},
+            {"number": 3, "option": "Ambos"},
+            {"number": 4, "option": "No tengo preferencia"}
+        ]
+    },
 
-    {"question_desc": "¿Cual es tu opinion sobre la legalizacion de la marihuana?",
-     "opciones": [
-         {"number": 1, "option": "A favor"},
-         {"number": 2, "option": "En contra"},
-         {"number": 3, "option": "Neutral"},
-         {"number": 4, "option": "No estoy seguro/a"}
-     ]},
-
-    {"question_desc": "¿Cual es tu opinion sobre la legalizacion de la eutanasia?",
-     "opciones": [
-         {"number": 1, "option": "A favor"},
-         {"number": 2, "option": "En contra"},
-         {"number": 3, "option": "Neutral"},
-         {"number": 4, "option": "No estoy seguro/a"}
-     ]},
-
-    {"question_desc": "¿Cual es tu opinion sobre la legalizacion de la pena de muerte?",
-     "opciones": [
-         {"number": 1, "option": "A favor"},
-         {"number": 2, "option": "En contra"},
-         {"number": 3, "option": "Neutral"},
-         {"number": 4, "option": "No estoy seguro/a"}
-     ]},
 
     {"question_desc": "¿Cual es tu tipo de comida favorita?",
      "opciones": [
@@ -109,11 +106,19 @@ preguntas_predefinidas=[
      ]}
 ]
 
-preguntas=Question.objects.all()
 nombres_votaciones=["Votacion 1", "Votacion 2", "Votacion 3", "Votacion 4", "Votacion 5", "Votacion 6", "Votacion 7", "Votacion 8", "Votacion 9", "Votacion 10"]
 local_url = getattr(settings, 'BASEURL', '127.0.0.1:8000')
+fake = Faker("es_ES")
+
+preguntas=Question.objects.all()
 votaciones=Voting.objects.all()
 usuarios=User.objects.all()
+
+comando_drop_db='psql -c "DROP DATABASE decidedb"'
+comando_completo_drop="sudo su - postgres -c '{O}'".format(O=comando_drop_db)
+
+comando_create_db='psql -c "CREATE DATABASE decidedb owner decideuser"'
+comando_completo_create="sudo su - postgres -c '{O}'".format(O=comando_create_db)
 
 def cambiar_url_local(url_nueva):
     ruta_archivo_local_settings="./local_settings.py"
@@ -147,11 +152,22 @@ if __name__ == '__main__':
             print("Url cambiada con exito")
     else:
         print("Url no cambiada")
+    req=input("¿Desea instalar los requerimientos? (si/no): ")
+    if req=="si":
+        print("Instalando requerimientos...")
+        os.system("pip install -r ../requirements.txt")
+        print("Requerimientos instalados",end="\n\n")
+    else:
+        print("Requerimientos no instalados",end="\n\n")
 
     migr=input("¿Desea aplicar las migraciones? (si/no): ")
     if migr=="si":
         print("Aplicando migraciones...")
         time.sleep(2)
+        os.system("find . -path '*/migrations/*.py' -not -name '__init__.py' -delete")
+        os.system("find . -path '*/migrations/*.pyc'  -delete")
+        os.system(comando_completo_drop)
+        os.system(comando_completo_create)
         os.system("python manage.py makemigrations")
         os.system("python manage.py migrate")
         print("Migraciones aplicadas",end="\n\n")
@@ -174,10 +190,15 @@ if __name__ == '__main__':
         for _ in range(50):
             user= User.objects.create_user(username=fake.user_name(), email=fake.email(),password="1234")
             print(f"Usuario creado: {user.username}")
-        admin = User.objects.create_superuser(username="admin", email="a@a.com",password="admin")
-        if admin:
-            print(f"Usuario admin creado: Usuario= {admin.username} Contraseña = admin")
-
+        ad=input("¿Desea crear un administrador? (si/no):" )
+        if ad=="si":
+            nombre=input("Introduzca el nombre del usuario administrador: ")
+            gmail=input("Introduzca el email del usuario administrador: ")
+            contraseña=input("Introduzca la contraseña del usuario administrador: ")
+            admin = User.objects.create_superuser(username=nombre, email=gmail,password=contraseña)
+            print(f"Usuario admin creado: Usuario= {admin.username} Contraseña = {contraseña}")
+        else:
+            pass
         print("Usuarios creados con exito",end="\n\n")
         time.sleep(2)
         print("Creando preguntas...")
@@ -236,6 +257,6 @@ if __name__ == '__main__':
         print("Base de datos no poblada")
         respuesta=input("¿Desea ejecutar la aplicación? (si/no):")
         if respuesta=="si":
-            os.system("python manage.py runserver")
+            os.system("python manage.py runserver " + local_url.replace("http://",""))
         else:
             print("Aplicación no ejecutada")
