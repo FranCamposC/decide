a31e48b95a444c8e651200f6f5ddff83fac25332
mariams73 <marmarmar73@alum.us.es>
2023-12-19 19:53:20 +0100

    fix: testing changes in launch.sh

--- decide/launch.sh
+++ decide/launch.sh
@@ -3,6 +3,8 @@ cd decide/
 cp local_settings.deploy.py local_settings.py
 ./manage.py createsuperuser --noinput
 ./manage.py collectstatic --noinput
+./manage.py migrate --fake
+./manage.py migrate --fake-initial
 ./manage.py makemigrations
 ./manage.py migrate
 gunicorn -w 5 decide.wsgi:application --timeout=500
